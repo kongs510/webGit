b6886c3e609419b6f4ac8fc540d08d395225004c
kongs <yoursitup90@gmail.com>
2020-09-08 14:17:14 +0900

    spring03-  list.jsp

---
+++ spring03/src/main/java/com/bit/framework/dept/model/DeptDaoImpl.java
@@ -0,0 +1,42 @@
+package com.bit.framework.dept.model;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.support.JdbcDaoSupport;
+
+import com.bit.framework.dept.model.entity.DeptVo;
+
+public class DeptDaoImpl extends JdbcDaoSupport implements DeptDao{
+
+	public List<DeptVo> selectAll() throws SQLException {
+		String sql="select * from dept";
+		return getJdbcTemplate().query(sql, new RowMapper<DeptVo>(){
+
+			@Override
+			public DeptVo mapRow(ResultSet rs, int rowNum) throws SQLException {
+				return new DeptVo(rs.getInt("deptno"),rs.getString("dname"),rs.getString("loc"));
+			}
+			
+		});
+	}
+
+	public void insertOne(DeptVo bean) throws SQLException {
+
+	}
+
+	public DeptVo selectOne(int key) throws SQLException {
+		return null;
+	}
+
+	public int updateOne(DeptVo bean) throws SQLException {
+		return 0;
+	}
+
+	public int deleteOne(int key) throws SQLException {
+		return 0;
+	}
+
+}
